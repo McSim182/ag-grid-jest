cc09abe43372b8800e5c4080d13ec01c628b2a4e
Maxim Shevchenko <maxim.shevchenko@spirent.com>
2023-07-03 16:52:02 +0300

    commented ag grid usage to pass tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -12,5 +13,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     expect(fixture.componentInstance).toBeTruthy();
+    const title = fixture.debugElement.query(By.css('h1'));
+    expect(title).toContain('Title');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AgGridModule } from '@ag-grid-community/angular';
+import { ColDef } from '@ag-grid-community/core';
 
 @Component({
   standalone: true,
@@ -9,5 +10,6 @@ import { AgGridModule } from '@ag-grid-community/angular';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ag-grid-jest';
+  rowData = [{ name: 'name1' }, { name: 'name2' }];
+  colDefs: Array<ColDef> = [{ field: 'name' }];
 }
